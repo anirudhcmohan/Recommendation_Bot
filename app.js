87f2eac29a4699beacef07fe519856b87cc635f5
Ani Mohan <anirudh.c.mohan@gmail.com>
2016-05-08 23:04:13 -0700

    Fixed bodyparser lib support

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ function sendTextMessage(sender, text) {
   });
 }
 
-app.post('/webhook/', function (req, res) {
+app.post('/webhook/', jsonParser, function (req, res) {
   messaging_events = req.body.entry[0].messaging;
   for (i = 0; i < messaging_events.length; i++) {
     event = req.body.entry[0].messaging[i];
