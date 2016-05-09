d289c83488d864c94d1f55b04c348ab7c73ee007
Ani Mohan <anirudh.c.mohan@gmail.com>
2016-05-08 22:44:38 -0700

    Echoing text

--- app.js
+++ app.js
@@ -14,6 +14,29 @@ app.get('/webhook/', function (req, res) {
   res.send('Error, wrong validation token');
 });
 
+var token = "EAAWiPbHonE8BABa1u7ozx5lZB1MZC8c6ZBYmwYeOrg04ZC0QqKkkiWAOyZCMigxZAmLZABOnJgi3ZC9NSSVZA1MZCrZCfuOwWD55dXAhIy28FTahAMZCXhtw0rwtcOFAWnrRKANiPa9vSmy41TCbMpsAuALJjxWG7CMPgH2aZBlA3jSEDZCQZDZD";
+
+function sendTextMessage(sender, text) {
+  messageData = {
+    text:text
+  }
+  request({
+    url: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: {access_token:token},
+    method: 'POST',
+    json: {
+      recipient: {id:sender},
+      message: messageData,
+    }
+  }, function(error, response, body) {
+    if (error) {
+      console.log('Error sending message: ', error);
+    } else if (response.body.error) {
+      console.log('Error: ', response.body.error);
+    }
+  });
+}
+
 app.post('/webhook/', function (req, res) {
   messaging_events = req.body.entry[0].messaging;
   for (i = 0; i < messaging_events.length; i++) {
@@ -21,7 +44,7 @@ app.post('/webhook/', function (req, res) {
     sender = event.sender.id;
     if (event.message && event.message.text) {
       text = event.message.text;
-      // Handle a text message from this sender
+      sendTextMessage(sender, "Echoing text"+ text);
     }
   }
   res.sendStatus(200);
