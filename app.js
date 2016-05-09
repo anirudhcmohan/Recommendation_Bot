ba5f02e2d9d15a7cea46f5f004fa16569b6cca06
Ani Mohan <anirudh.c.mohan@gmail.com>
2016-05-08 23:56:27 -0700

    logging sender info

--- app.js
+++ app.js
@@ -41,15 +41,17 @@ function sendTextMessage(sender, text) {
   });
 }
 
+
 app.post('/webhook/', function (req, res) {
   messaging_events = req.body.entry[0].messaging;
   for (i = 0; i < messaging_events.length; i++) {
     event = req.body.entry[0].messaging[i];
     sender = event.sender.id;
+    console.log(sender);
     if (event.message && event.message.text) {
       text = event.message.text;
       console.log(text);
-      sendTextMessage(sender, "Echoing text"+ text);
+      sendTextMessage(sender, "Echoing text: "+ text);
     }
   }
   res.sendStatus(200);
