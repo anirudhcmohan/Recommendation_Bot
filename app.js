500723cbe003714d63a209db08ec878ded1824ea
Ani Mohan <anirudh.c.mohan@gmail.com>
2016-05-08 22:35:07 -0700

    Setting up webhook

--- app.js
+++ app.js
@@ -7,6 +7,13 @@ app.get('/', function(req, res) {
 	res.send('Hello World');
 });
 
+app.get('/webhook/', function (req, res) {
+  if (req.query['hub.verify_token'] === 'this_is_a_sample_token') {
+    res.send(req.query['hub.challenge']);
+  }
+  res.send('Error, wrong validation token');
+})
+
 app.listen(app.get('port'), function(){
 	console.log('Example app listening on port 3000!');
 });
